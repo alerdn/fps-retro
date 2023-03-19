6288bfac31fffea1e6d63c666ea667b1aad5ae88
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2023-03-19 15:59:03 -0400

    :sparkles: hud com arma implementado

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-   private Camera _camera;
+    [SerializeField] private int _maxAmmo;
+    [SerializeField] private int _currentAmmo;
+
+    private Camera _camera;
 
     void Start()
     {
         _camera = GetComponentInChildren<Camera>();
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
@@ -20,15 +26,19 @@ public class PlayerAttack : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Ray ray = _camera.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
-            RaycastHit raycastHit;
+            if (_currentAmmo > 0)
+            {
+                Ray ray = _camera.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+                if (Physics.Raycast(ray, out RaycastHit raycastHit))
+                {
+                    Debug.Log($"Looking at {raycastHit.transform.name}");
+                }
+                else
+                {
+                    Debug.Log("Looking at nothing");
+                }
 
-            if (Physics.Raycast(ray, out raycastHit))
-            {
-                Debug.Log($"Looking at {raycastHit.transform.name}");
-            } else
-            {
-                Debug.Log("Looking at nothing");
+                _currentAmmo--;
             }
         }
     }
